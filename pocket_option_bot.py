1d4ce9bb301483ee5b4f20a6d40c2aa998522e2c
oleg1983252014-collab <oleg1983252014@gmail.com>
2026-03-10 09:46:58 +0200

    Add files via upload

--- pocket_option_bot.py
+++ pocket_option_bot.py
@@ -23,6 +23,7 @@ from datetime import datetime
 # ══════════════════════════════════════════
 #  НАЛАШТУВАННЯ — ВСТАВТЕ СВІЙ ТОКЕН
 # ══════════════════════════════════════════
+import os
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 
 bot = telebot.TeleBot(BOT_TOKEN)
